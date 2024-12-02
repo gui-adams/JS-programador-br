f32e49c48d2634b01811e9ebaabcc74533876491
gui-adams <guilherme.adams@live.com>
2024-12-02 17:43:48 -0300

    string

---
+++ JS_basico/9_string/script.js
@@ -0,0 +1,18 @@
+
+/*
+uma string pode ser criar tanto com aspas simples como aspas duplas
+
+ao adicionar uma aspas simples dentro da aspas duplicas o a string é marcada como aspas para (diferenciação)
+*/
+
+var numero = 8
+var str = "Valor 'number' " + numero  //no console.log azul ele vai ser número e preto equivaliente string
+
+// concatenar usando + para somar string a outra
+//metodo mais usado
+var str2 = "botafogo " + "campeão " + "da liberadores"
+
+console.log(str)
+console.log(str2)
+
+
