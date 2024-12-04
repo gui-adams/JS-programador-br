c664b8f0b90b8876279273dce5120fe3c1ccebe4
gui-adams <guilherme.adams@live.com>
2024-12-04 17:29:59 -0300

    string e alert

---
+++ JS_basico/10_string_em_numero/script.js
@@ -0,0 +1,16 @@
+
+/*
+Somar string trasformando em numero
+
+parseInt() Number inteiro
+parseFloat() Number flutuante casas decimais
+
+*/
+
+var a = "5.5";
+
+var b = "5";
+
+var c = parseFloat(a) + parseInt(b)
+
+console.log(c)
---
+++ JS_basico/11_alert_prompt/script.js
@@ -0,0 +1,19 @@
+
+/*
+Somar string trasformando em numero
+
+parseInt() Number inteiro
+parseFloat() Number flutuante casas decimais
+
+*/
+
+var nome = prompt("Qual é o seu nome")
+
+alert("Bom dia, " + nome)
+
+
+var numero = parseInt(prompt("Digite um número"))
+
+var dobro = numero + numero
+
+alert("O dobre de " + numero + " é " + dobro)
