5c4ddedea2c819e30a746a410c7c46f46aaef2c1
root <root@neofogo>
2024-12-10 06:46:53 -0300

    Operador ternario

---
+++ JS_basico/13_operador_ternario/script.js
@@ -0,0 +1,14 @@
+
+/*
+operador ternario é = ? 
+esse operador ? é maior ou igual ? 
+o if (teoria) é ?
+o else é :
+é usado para substituir o if em algumas questões
+*/
+
+var idade = 19;
+
+var pode = idade >= 18 ? true : false; // se idade for maior ou igual 18 é true "se não" é falso
+
+console.log(pode)
